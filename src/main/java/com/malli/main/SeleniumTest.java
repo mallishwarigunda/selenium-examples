3b8b85a63b6e44c3ff802ce7c7eb41fa89f4fb60
Mallikarjun Gunda <mallikarjungunda@MallikarjunsMBP.fios-router.home>
2022-01-22 19:25:47 -0500

    second commit

--- src/main/java/com/malli/main/SeleniumTest.java
+++ src/main/java/com/malli/main/SeleniumTest.java
@@ -2,6 +2,7 @@ package com.malli.main;
 
 public class SeleniumTest {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+
+        System.out.println(" selenium examples");
     }
 }
